d558ffec56819a6b9ed1963b92edf71ec41ea112
yun <sys394880@gmail.com>
2020-06-20 21:12:08 +0900

    update 2020-06-20 21:12

--- ceo/static/js/write.js
+++ ceo/static/js/write.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
     $('.summernote').summernote({
         height: 150
     });
+
+    $('.recipe_form').submit((e) =>{
+        e.preventDefault();
+        var recipeName = document.querySelector('#recipe_name').value;
+        console.log(recipeName);
+        var nutrition = document.querySelectorAll('.note-editable')[0].innerHTML;
+        console.log(nutrition);
+        var recipeDoc = document.querySelectorAll('.note-editable')[1].innerHTML;
+        console.log(recipeDoc);
+        var foodStuff = document.getElementById('foodStuff').value;
+        console.log(foodStuff);
+    });
+
+    $('.storage_form').submit((e) => {
+        e.preventDefault();
+    });
 });
--- ceo/templates/js/write.js
+++ ceo/templates/js/write.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     $('.summernote').summernote({
         height: 150
     });
+
+    $('.recipe_form').submit((e) =>{
+        e.preventdefault();
+        console.log('Submit!!!!');
+    });
 });
