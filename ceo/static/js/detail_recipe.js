4d2d0389ea0771c677c5083d00346930957ca6e4
yun <sys394880@gmail.com>
2020-06-27 14:45:41 +0900

    서비스 시현 테스트 및 레시피 정보 페이지 부분 기능 수정

--- ceo/static/js/detail_recipe.js
+++ ceo/static/js/detail_recipe.js
@@ -1,10 +1,11 @@
-
+var price = 0;
 
 $("#food-select").change(function() {
     if($(this).val() != "재료선택"){
         $("#food-meta").css("display","block");
         var food_price = parseInt($("#food-select > option:selected").attr("price"));
-        var price = parseInt($("#total").text()) + food_price;
+        // var price = parseInt($("#total").text()) + food_price;
+        price += food_price;
         $("#total").text(price);
         var number = $(".food").length + 1;
         var tag = "<li class='food'>\
@@ -17,10 +18,13 @@ $("#food-select").change(function() {
     }
 });
 $(document).on("click",".delete-food",function(){
+    price -= $(this).prev("span.price").text().replace("원", "");
     $(this).parent("li").remove();
     if($(".food").length == 0){
         $("#food-meta").css("display","none");
 
+    }else{
+        $("#total").text(price);
     }
     var n = 1;
     for(var num of $(".food-num")){
@@ -31,7 +35,8 @@ $(document).on("click",".delete-food",function(){
 });
 
 $(document).on("change",".num",function(){
-    $("#total").text(getToatal());
+    price = getToatal();
+    $("#total").text(price);
 });
 function getToatal(){
     var sum = 0;
